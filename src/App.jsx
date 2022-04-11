3b047ed04d7827a29ca3452cc1c7f2e474980870
Kenie B <kbett001@gmail.com>
2022-04-11 03:18:18 -0400

    Base routing added (react-router-dom v6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Home from "./Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Container fluid className="p-0">
-        <Home />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
       </Container>
     </div>
   );
