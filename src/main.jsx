e9bc0d2db67ab7dceacc850a7b02add79e222e0e
Kenie B <kbetts001@gmail.com>
2022-09-26 07:26:24 -0400

    Cleaned CSS / Refactor to SASS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import "./_customCSS/index.css";
 import App from "./App";
 
 //basename={"https://www.kenie-betts.com/"}
