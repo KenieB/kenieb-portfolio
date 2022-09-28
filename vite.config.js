32359a0b8a37d1e9230eb211d38913ae67a46c90
Kenie B <kbetts001@gmail.com>
2022-09-28 02:51:59 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/kenieb-portfolio/",
   plugins: [react()],
-  build: {
+  //build: {
     //minify: false
-  }
+  //}
 });
